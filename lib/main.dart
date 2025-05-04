dc7b14622bb2efb5766e8fb25b95ab9a1e3747a8
Qylahrzk <nuraqilahnur@gmail.com>
2025-05-04 16:46:28 +0800

    Improve Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:spendlytic/screens/splash_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+// Import your screens
+import 'package:spendlytic/screens/splash_screen.dart';
+import 'package:spendlytic/screens/login_screen.dart';
+import 'package:spendlytic/screens/signup_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive (important!)
+  await Hive.initFlutter();
+
   runApp(const SpendlyticApp());
 }
 
@@ -15,8 +25,20 @@ class SpendlyticApp extends StatelessWidget {
       title: 'Spendlytic',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
       ),
+
+      // Initial screen
       home: const SplashScreen(),
+
+      // Define named routes
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen()
+      },
     );
   }
 }
