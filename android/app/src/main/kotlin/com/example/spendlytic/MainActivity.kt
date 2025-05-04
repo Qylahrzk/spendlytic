fa2dd27c8b396ea16e1cfcb69c38e2e31d377c6a
Qylahrzk <nuraqilahnur@gmail.com>
2025-05-04 15:36:01 +0800

    first_commit_1

---
+++ android/app/src/main/kotlin/com/example/spendlytic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spendlytic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/common/color_extension.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+extension CustomColors on ColorScheme {
+  Color get primaryColor => const Color(0xFF4A90E2);
+  Color get secondaryColor => const Color(0xFF50E3C2);
+  Color get dangerColor => const Color(0xFFFF5A5F);
+  Color get successColor => const Color(0xFF7ED321);
+  Color get textPrimary => Colors.black87;
+  Color get textSecondary => Colors.grey;
+}
